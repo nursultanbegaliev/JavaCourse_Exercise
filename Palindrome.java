2c4227c7c8de20727b47863bdacf817093f06ea1
Jonokoy <59413790+nursultanbegaliev@users.noreply.github.com>
2020-03-13 14:34:58 +0600

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+package week6_Exerscieses;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        int num = number;
+        while (num > 1) {
+
+        }
+    }
+
+}
